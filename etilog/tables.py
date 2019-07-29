5878d497c4f4210796dce29e71bc2807fa78b2c2
daim <hodeld@rocketmail.com>
2019-07-29 19:45:21 +0200

    impact table

--- etilog/tables.py
+++ etilog/tables.py
@@ -3,19 +3,41 @@ Created on 24 Jul 2019
 
 @author: daim
 '''
+#django 
+from django.utils.html import mark_safe
+#3rd app
 import django_tables2 as tables
+#models
 from .models import ImpactEvent
 
+class DefWidthColumn(tables.Column):
+
+    def __init__(self, classname=None, *args, **kwargs):
+        self.classname=classname
+        super(DefWidthColumn, self).__init__(*args, **kwargs)
+
+    def render(self, value):
+        return mark_safe("<div class='" + self.classname + "' >" +value+"</div>")
+    
+    
 class ImpEvTable(tables.Table):
     '''
     basic table for impact events
     '''
 
-    id = tables.Column( linkify = True )
+    id = tables.Column(linkify = True )
+    #source_url = DefWidthColumn(classname='limited_width')
+       
+    
     class Meta:
         model = ImpactEvent
         attrs = {'class': 'table table-hover table-sm' } #bootstrap4 classes
         exclude = ('created_at', 'updated_at', )
         fields = ('id', 'date_impact', 'company', 'country', 
-                  'sust_category', 'all_tags', 'reference', 'source_link', 'comment' )
+                  'sust_category', 'all_tags', 'reference', 'source_url', 'comment' )
+    
+    def render_source_url(self, value):
+        val_short = str(value)[:20]
+        return  val_short
+    
     
--- etilog/views.py
+++ etilog/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.urls import reverse
+
+#from 3rd apps
+from django_tables2 import RequestConfig
+
 #models
 from .models import ImpactEvent
 #tables
@@ -17,6 +21,8 @@ def startinfo(request):
 def overview_impevs(request):
     
     table = ImpEvTable(ImpactEvent.objects.all())
+    #table.order_by = 'start'
+    RequestConfig(request, paginate={'per_page': 20}).configure(table) 
     
     return render(request, 'etilog/impactevents_overview.html', {'table': table})
 
