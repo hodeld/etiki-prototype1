e43fc7de7dde0a21853d01298c652b41abc81ae1
Damian <42800152+hodeld@users.noreply.github.com>
2020-04-01 15:49:18 +0200

    comment settings

--- etikiptype1/settings/__init__.py
+++ etikiptype1/settings/__init__.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#used for heroku local:
+#used for heroku local: (in .env file)
 SECRET_KEY = os.getenv('SECRET_KEY', 'Optional default value')
 
 db_user = os.getenv('DB_USER', 'Optional default value')
