7afa711910a8397854c261d946f93767e96c090f
Damian <42800152+hodeld@users.noreply.github.com>
2020-04-09 17:11:53 +0200

    to pycharm

--- etikiptype1/settings/__init__.py
+++ etikiptype1/settings/__init__.py
@@ -15,7 +15,8 @@ import sys
 
 import django_heroku 
 
-from secrets import *
+from etikiptype1.settings.secrets import *
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -26,6 +27,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY', 'Optional default value')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
