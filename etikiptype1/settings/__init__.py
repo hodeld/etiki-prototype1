b5af9c5f2351ad4bf051eab10e23080d1c9798b4
Damian <42800152+hodeld@users.noreply.github.com>
2020-06-12 10:26:15 +0200

    memcachedCloud

--- etikiptype1/settings/__init__.py
+++ etikiptype1/settings/__init__.py
@@ -123,38 +123,23 @@ LOGIN_REDIRECT_URL = 'etilog:home'
 def get_cache():
     import os
     try:
-        servers = os.environ['MEMCACHIER_SERVERS']
-        username = os.environ['MEMCACHIER_USERNAME']
-        password = os.environ['MEMCACHIER_PASSWORD']
+        servers = os.environ['MEMCACHEDCLOUD_SERVERS'].split(',')
+        username = os.environ['MEMCACHEDCLOUD_USERNAME']
+        password = os.environ['MEMCACHEDCLOUD_PASSWORD']
         return {
             'default': {
-                'BACKEND': 'django.core.cache.backends.memcached.PyLibMCCache',
+                'BACKEND': 'django.core.cache.backends.locmem.LocMemCache'
+            },
+            'memcache': {
+                'BACKEND': 'django_bmemcached.memcached.BMemcached',
                 # TIMEOUT is not the connection timeout! It's the default expiration
                 # timeout that should be applied to keys! Setting it to `None`
                 # disables expiration.
                 'TIMEOUT': 60*60,
                 'LOCATION': servers,
                 'OPTIONS': {
-                    'binary': True,
                     'username': username,
                     'password': password,
-                    'behaviors': {
-                        # Enable faster IO
-                        'no_block': True,
-                        'tcp_nodelay': True,
-                        # Keep connection alive
-                        'tcp_keepalive': True,
-                        # Timeout settings
-                        'connect_timeout': 2000,  # ms
-                        'send_timeout': 750 * 1000,  # us
-                        'receive_timeout': 750 * 1000,  # us
-                        '_poll_timeout': 2000,  # ms
-                        # Better failover
-                        'ketama': True,
-                        'remove_failed': 1,
-                        'retry_timeout': 2,
-                        'dead_timeout': 30,
-                    }
                 }
             }
         }
