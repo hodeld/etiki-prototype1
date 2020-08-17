e75a3741d65db6869411730f4070e87ec0009726
Damian <42800152+hodeld@users.noreply.github.com>
2020-08-17 12:54:16 +0200

    refactoring typeahead and tagsinput

---
+++ etilog/static/etilog/js/base_typeahead.js
@@ -0,0 +1,40 @@
+$(document).ready(function () {
+
+});
+
+//initialize BLOODHOUND
+function getBloodhoundOpt(field_url) {
+    optDict = {
+        datumTokenizer: Bloodhound.tokenizers.obj.whitespace('name'), //obj.whitespace('name') -> data needs to be transformed
+        queryTokenizer: Bloodhound.tokenizers.whitespace,
+        //identify: function(obj) { return obj.id; }, //to get suggestion by ID -> not used and breaks typahead!
+        prefetch: {
+            url: field_url, // url set in html
+            cache: true	 // defaults to true -> for testing
+        },
+    };
+    return optDict
+}
+
+var bldhndOptComp = new getBloodhoundOpt(companies_url);
+var companies = new Bloodhound(bldhndOptComp);
+
+var optCountries = new getBloodhoundOpt(countries_url);
+var countries = new Bloodhound(optCountries);
+
+var optReferences = new getBloodhoundOpt(references_url);
+var references = new Bloodhound(optReferences);
+
+var optTopics = new getBloodhoundOpt(tags_url);
+var tags = new Bloodhound(optTopics);
+
+//TypeaheadOpt
+function getTypeaheadOpt(name, source, limitSugg=3) {
+    const optDict = {
+        name: name,
+        source: source,
+        display: 'name',
+        limit: limitSugg,
+    };
+    return optDict
+}
